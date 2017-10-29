f6c90a55b88882b0ab7cd9d1d209d810e9fc0550
Kenan Berbic <kenan.berbic@symphony.is>
2017-10-29 02:22:12 +0200

    Clean package json, remove index, add npm ignore

--- src/App.js
+++ src/App.js
@@ -19,94 +19,84 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">react-moment-input</h1>
         </header>
-          <div style={{display: "table", margin:"0 auto"}}>
-              <p className="App-intro">
-                  Data picker simple
-              </p>
-              <MomentInput
-                  format="YYYY-MM-DD HH:mm"
-                  defaultValue={this.state.value}
-                  defaultTime="03:00"
-                  options={true}
-                  readOnly={false}
-                  onSave={(value)=> {this.setState({value})}} />
-          </div>
-          <div style={{display: "table", margin:"0 auto"}}>
-              <p className="App-intro">
-                  Min and max date <br/>
-                  2019-10-05 10:45 PM/AM
-              </p>
-              <MomentInput
-                  max={moment("2019-10-05 22:45")}
-                  min={moment()}
-                  format="YYYY-MM-DD hh:mm AA"
-                  translations={{HOURS:"Sati", MINUTES:"Minute",YEARS:"Godine", DATE:"Dani", TIME:"Vrijeme", MONTHS_OCTOBER:"Oktobar", DAYS_MON:"PON"}}
-                  options={true}
-                  readOnly={false}
-                  icon={true}
-                  onChange={(date)=> {}} />
-          </div>
-          <div style={{display: "table", margin:"0 auto"}}>
-              <p className="App-intro">
-                  Different days order
-              </p>
-              <MomentInput
-                  format="YYYY-MM-DD HH:mm"
-                  options={true}
-                  readOnly={true}
-                  daysOfWeek={['Mon','Tue','Wed','Thu','Fri','Sat','Sun']}
-                  icon={true}
-                  onChange={(date)=> {}} />
-          </div>
-          <div style={{display: "table", margin:"0 auto"}}>
-              <p className="App-intro">
-                  Input disabled
-              </p>
-              <MomentInput
-                  format="YYYY-MM-DD HH:mm"
-                  options={true}
-                  readOnly={true}
-                  icon={true}
-                  onChange={(date)=> {}} />
-          </div>
-          <div style={{display: "table", margin:"0 auto"}}>
-              <p className="App-intro">
-                  Month select disabled
-              </p>
-              <MomentInput
-                  format="YYYY-MM-DD"
-                  options={false}
-                  readOnly={false}
-                  monthSelect={false}
-                  icon={true}
-                  onChange={(date)=> {}} />
-          </div>
-          <div style={{display: "table", margin:"0 auto"}}>
-              <p className="App-intro">
-                  Only time
-              </p>
-              <MomentInput
-                  format="HH:mm:ss"
-                  options={false}
-                  value={this.state.value}
-                  readOnly={false}
-                  icon={true}
-                  tab={1}
-                  onChange={(date)=> (value)=> {this.setState({value})}} />
-          </div>
-          <div style={{display: "table", margin:"0 auto"}}>
-              <p className="App-intro">
-                  Year, calendar and today
-              </p>
-              <MomentInput
-                  format="YYYY-MM-DD"
-                  options={false}
-                  readOnly={false}
-                  today={true}
-                  icon={true}
-                  tab={2}
-                  onChange={(date)=> {}} />
-          </div>
+          <table style={{margin:"0 auto"}}>
+              <tbody>
+              <tr>
+                  <td>Data picker simple: </td>
+                  <td><MomentInput
+                      inputStyle={{width:"201px"}}
+                      format="YYYY-MM-DD HH:mm"
+                      defaultValue={this.state.value}
+                      defaultTime="03:00"
+                      options={true}
+                      readOnly={false}
+                      onSave={(value)=> {this.setState({value})}} /></td>
+              </tr>
+              <tr>
+                  <td> Min and max date: </td>
+                  <td><MomentInput
+                      max={moment("2019-10-05 22:45")}
+                      min={moment()}
+                      format="YYYY-MM-DD hh:mm AA"
+                      translations={{HOURS:"Sati", MINUTES:"Minute",YEARS:"Godine", DATE:"Dani", TIME:"Vrijeme", MONTHS_OCTOBER:"Oktobar", DAYS_MON:"PON"}}
+                      options={true}
+                      readOnly={false}
+                      icon={true}
+                      onChange={(date)=> {}} /></td>
+              </tr>
+              <tr>
+                  <td> Different days order: </td>
+                  <td><MomentInput
+                      format="YYYY-MM-DD HH:mm"
+                      options={true}
+                      readOnly={true}
+                      daysOfWeek={['Mon','Tue','Wed','Thu','Fri','Sat','Sun']}
+                      icon={true}
+                      onChange={(date)=> {}} /></td>
+              </tr>
+              <tr>
+                  <td>Input disabled: </td>
+                  <td><MomentInput
+                      format="YYYY-MM-DD HH:mm"
+                      options={true}
+                      readOnly={true}
+                      icon={true}
+                      onChange={(date)=> {}} /></td>
+              </tr>
+              <tr>
+                  <td>Month select disabled: </td>
+                  <td><MomentInput
+                      format="YYYY-MM-DD"
+                      options={false}
+                      readOnly={false}
+                      monthSelect={false}
+                      icon={true}
+                      onChange={(date)=> {}} /></td>
+              </tr>
+              <tr>
+                  <td>Only time: </td>
+                  <td><MomentInput
+                      format="HH:mm:ss"
+                      options={false}
+                      value={this.state.value}
+                      readOnly={false}
+                      icon={true}
+                      tab={1}
+                      onChange={(date)=> (value)=> {this.setState({value})}} /></td>
+              </tr>
+              <tr>
+                  <td>Year, calendar and today: </td>
+                  <td><MomentInput
+                      format="YYYY-MM-DD"
+                      options={false}
+                      readOnly={false}
+                      today={true}
+                      icon={true}
+                      tab={2}
+                      onChange={(date)=> {}} /></td>
+              </tr>
+              </tbody>
+          </table>
           </div>
 
     );
