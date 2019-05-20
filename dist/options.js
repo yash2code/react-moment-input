debbf469b352d4702c8c4ebe5af452dd92baded1
yash <yash240196@gmail.com>
2019-05-20 10:24:05 +0530

    prevents auto submit for formik

--- dist/options.js
+++ dist/options.js
@@ -1,7 +1,7 @@
 "use strict";
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
 var _react = require("react");
@@ -11,31 +11,35 @@ var _react2 = _interopRequireDefault(_react);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = function (_ref) {
-    var activeTab = _ref.activeTab,
-        onActiveTab = _ref.onActiveTab,
-        translations = _ref.translations;
-    return _react2.default.createElement(
-        "div",
-        { className: "options" },
-        _react2.default.createElement(
-            "button",
-            {
-                tabIndex: -1,
-                className: "ion-ios-calendar im-btn" + (activeTab === 0 || activeTab === 2 ? " is-active" : ""),
-                onClick: function onClick() {
-                    onActiveTab(0);
-                } },
-            translations.DATE || "Date"
-        ),
-        _react2.default.createElement(
-            "button",
-            {
-                tabIndex: -1,
-                className: "ion-ios-clock im-btn" + (activeTab === 1 ? " is-active" : ""),
-                onClick: function onClick() {
-                    onActiveTab(1);
-                } },
-            translations.TIME || "Time"
-        )
-    );
+  var activeTab = _ref.activeTab,
+      onActiveTab = _ref.onActiveTab,
+      translations = _ref.translations;
+  return _react2.default.createElement(
+    "div",
+    { className: "options" },
+    _react2.default.createElement(
+      "button",
+      {
+        type: "button",
+        tabIndex: -1,
+        className: "ion-ios-calendar im-btn" + (activeTab === 0 || activeTab === 2 ? " is-active" : ""),
+        onClick: function onClick() {
+          onActiveTab(0);
+        }
+      },
+      translations.DATE || "Date"
+    ),
+    _react2.default.createElement(
+      "button",
+      {
+        type: "button",
+        tabIndex: -1,
+        className: "ion-ios-clock im-btn" + (activeTab === 1 ? " is-active" : ""),
+        onClick: function onClick() {
+          onActiveTab(1);
+        }
+      },
+      translations.TIME || "Time"
+    )
+  );
 };
--- src/lib/options.js
+++ src/lib/options.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React from "react";
 
-export default ({activeTab, onActiveTab, translations}) => (<div className="options">
-        <button
-            tabIndex={-1}
-            className={"ion-ios-calendar im-btn" + ((activeTab===0 || activeTab===2) ? " is-active" : "")}
-            onClick={()=> {onActiveTab(0)}}>{translations.DATE || "Date"}
-        </button>
-        <button
-            tabIndex={-1}
-            className={"ion-ios-clock im-btn" + (activeTab===1 ? " is-active" : "")}
-            onClick={()=> {onActiveTab(1)}}>{translations.TIME || "Time"}
-        </button>
-    </div>
+export default ({ activeTab, onActiveTab, translations }) => (
+  <div className="options">
+    <button
+      type="button"
+      tabIndex={-1}
+      className={
+        "ion-ios-calendar im-btn" +
+        (activeTab === 0 || activeTab === 2 ? " is-active" : "")
+      }
+      onClick={() => {
+        onActiveTab(0);
+      }}
+    >
+      {translations.DATE || "Date"}
+    </button>
+    <button
+      type="button"
+      tabIndex={-1}
+      className={"ion-ios-clock im-btn" + (activeTab === 1 ? " is-active" : "")}
+      onClick={() => {
+        onActiveTab(1);
+      }}
+    >
+      {translations.TIME || "Time"}
+    </button>
+  </div>
 );
